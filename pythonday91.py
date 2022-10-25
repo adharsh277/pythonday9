16fd4e1eeca91b2808816e16873ab4c29ebbfc6d
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-25 17:19:23 +0530

    Add files via upload

---
+++ pythonday91.py
@@ -0,0 +1,51 @@
+from cgi import print_environ
+from tkinter import W
+
+
+w = "before coming to lovely proffessional university it was supercool but coming to hear it makes us very confident and we can improve our language and try to learn something new"
+x = 1676, 890076, 7124, 9089, 000
+y = "these are the numbers that a men could see from a cloud but it still very hard to get"
+z = "and i like a girl from maharstra she is typical and talks less i dont know why! but why we csn see where it will reaches"
+print(w)
+print(x)
+print(y)
+print(z)
+print(w.upper())
+print(z.upper())
+print(y.upper())
+print(w.lower())
+print(y.lower())
+print(z.lower())
+print(w.split())
+print(z.split())
+print(y.replace("get", "see"))
+print(z.replace("typical", "rare"))
+print(w.replace("supercool", "breathtaking"))
+print(len(x))
+print(len(y))
+print(len(z))
+print(len(w))
+# python operatrs
+print(5624 == 6273)
+print(23541+534632)
+print(86345-32563)
+print(25342*566)
+print(354**345)
+print(255 % 66)
+print(4577/356)
+print(65686//54)
+
+print(35 != 44)
+print(564 == 56)
+print(4 > 6)
+print(45 < 8)
+print(56 <= 67)
+print(788+4)
+print()
+x = print
+print('lionel messi')
+print('messi:')
+print("fifa is ours")
+print('nothing can defeat us')
+print('so qatar we are coming')
+print(x)
